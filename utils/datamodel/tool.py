4796de7c3f8a46367ad5845ec18365e8596a1672
Hayden Welch <hwelch@finelines-engineering.com>
2024-08-03 16:41:36 -0400

    add caching to the Tool base class.
    Expensive initilizations can contribute to slowdowns in large toolboxes as each tool subclass has its initilizer called on toolbox load and toolbox refresh.
    
    Each tool will also re-initilize when opened, and during update operations to parameters. So storing the result of an init into a static cache will actually bring the behavior of the tool more in line with a standard Python object

--- utils/datamodel/tool.py
+++ utils/datamodel/tool.py
@@ -1,17 +1,43 @@
 import arcpy
 import os
+import pathlib
+from pathlib import Path
 
 from typing import Any
 from abc import ABC
 
 class Tool(ABC):
+    
+    # Set up a cache for the initialized values
+    # An expensive initialization will massively slow down large toolboxes
+    # because the init is called for every tool in the toolbox on load and every
+    # time the tool opened.
+    # 
+    # When using the Tool cache, make sure that all initiliazation values are
+    # immutable or will not change during the life of the toolbox.
+    # Things like the project, named databases, etc. 
+    # are good candidates for caching in the base tool class
+    # Avoid caching things like the current map, layers, etc.
+    # in the base class as they may change during the life of the toolbox
+    # These values should be initiliazed in the tool subclasses only when they
+    # are needed.
+    init_cache: dict[str, Any] = {}
+    
     """
     Base class for all tools that use python objects to build parameters
     """
-    def __init__(self) -> None:
+    def __init__(self, *, invalidate_cache=False) -> None:
         """
-        Tool Description
+        Base initializer for all tools
+        
+        @param invalidate_cache: If True, the cache will be invalidated and the
+                                    tool will be re-initialized
         """
+        # If a tool has already been initialized, use the cache
+        if Tool.init_cache and not invalidate_cache:
+            self.__dict__.update(Tool.init_cache)
+            return
+        
         # Tool parameters
         self.label = "Tool"
         self.description = "Base class for all tools"
@@ -21,11 +47,14 @@ class Tool(ABC):
         # Project variables
         self.project = arcpy.mp.ArcGISProject("CURRENT")
         self.project_location = self.project.homeFolder
-        self.project_name = os.path.basename(self.project_location)
+        self.project_name = Path(self.project.filePath).stem
         
         # Database variables
         self.default_gdb = self.project.defaultGeodatabase
         self.databases = self.project.databases
+        
+        # Cache after first initialization
+        Tool.init_cache = self.__dict__
         return
     
     def getParameterInfo(self) -> list[arcpy.Parameter]: ...
@@ -34,3 +63,4 @@ class Tool(ABC):
     def updateMessages(self, parameters: list[arcpy.Parameter]) -> None: ...
     def execute(self, parameters: list[arcpy.Parameter], messages:list[Any]) -> None: ...
     def postExecute(self, parameters: list[arcpy.Parameter]) -> None: ...
+    
